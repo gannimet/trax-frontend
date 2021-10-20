d093298cfc7883e16d3198cf4f591d59c04c6001
Richard Wotzlaw <r.wotzlaw@gmail.com>
2021-10-20 20:57:43 +0200

    react setup with prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'prettier',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+    project: 'tsconfig.eslint.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['prettier', '@typescript-eslint', 'react', 'import'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    indent: 'off',
+    '@typescript-eslint/indent': 'off',
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'export' },
+      { blankLine: 'always', prev: '*', next: 'class' },
+      { blankLine: 'always', prev: '*', next: 'return' },
+    ],
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  bracketSpacing: true,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always'
+};
