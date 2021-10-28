9fbfddc645ff37a85624157b6a0df21ef358ed68
Richard Wotzlaw <r.wotzlaw@gmail.com>
2021-10-28 11:02:31 +0200

    better responsive behavior of ticket item in list

--- src/components/SprintTicketList/SprintTicketListItem/sprint-ticket-list-item.tsx
+++ src/components/SprintTicketList/SprintTicketListItem/sprint-ticket-list-item.tsx
@@ -12,13 +12,20 @@ const SprintTicketListItem = React.memo<SprintTicketListItemProps>(
       <List.Item
         className={`sprint-ticket-list-item ${ticket.ticketType.name.toLowerCase()}`}
       >
-        <Row style={{ width: '100%' }} align="middle">
-          <Col span={21}>
+        <Row style={{ width: '100%' }} gutter={8} wrap={false} align="middle">
+          <Col flex="none">
+            <Link>{ticket.issueNumber}</Link>
+          </Col>
+          <Col flex="auto">
+            <Text ellipsis={{ tooltip: ticket.title }}>
+              {ticket.title} Der YouTuber »Drachenlord« wird seit Jahren von
+              einem Mob gequält, bedroht und belästigt. Nun hat er sich gewehrt
+              und wurde zu einer Haftstrafe verurteilt. Ein katastrophales
+              Versagen von Justiz, Medien und Gesellschaft.
+            </Text>
+          </Col>
+          <Col flex="none" style={{ textAlign: 'right' }}>
             <Space>
-              <Link>{ticket.issueNumber}</Link>
-              <Text ellipsis={{ tooltip: ticket.title }} className="title-text">
-                {ticket.title}
-              </Text>
               {ticket.tags?.length > 0 &&
                 ticket.tags.map((tag) => {
                   return (
@@ -27,17 +34,14 @@ const SprintTicketListItem = React.memo<SprintTicketListItemProps>(
                     </Tag>
                   );
                 })}
-            </Space>
-          </Col>
-          <Col span={3} style={{ textAlign: 'right' }}>
-            <Space>
               {ticket.assignee && <UserAvatar user={ticket.assignee} />}
               {ticket.estimate != null && (
                 <Badge
                   count={ticket.estimate}
                   style={{
-                    backgroundColor: '#aaa',
-                    color: '#fff',
+                    border: '1px solid #999',
+                    backgroundColor: '#eee',
+                    color: '#000',
                     width: '30px',
                   }}
                 />
